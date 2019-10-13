d43072dc37295d784dfe6a90e1c35cc9ce80dcba
rgarcia <rgarcia@transparentcdn.com>
2019-10-13 19:22:19 +0200

    - Developing structure. - Build Skeleton - Add some styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Typography, Row, Col } from 'antd';
+import background from './background.jpg'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const { Title } = Typography;
+
+    return (
+      <div className="App" style={{ backgroundImage: `url(${background})` }}>
+        <Row>
+          <Col span={24}>
+            <Title>LA LONJA ONLINE ESPAÑOLA</Title>
+            <Title level={3}>Compra y vende productos agrícolas sin intermediarios</Title>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={2} lg={8}></Col>
+          <Col xs={20} lg={8}>
+            <Row type="flex" justify="center" gutter={16} >
+              <Col span={12}>
+                <div className="container-link blue-background">
+                  <Title level={2}>Publicar demanda</Title>
+                </div>
+              </Col>
+              <Col span={12}>
+                <div className="container-link green-background" >
+                  <Title level={2}>Ofrecer producto</Title>
+                </div>
+              </Col>
+            </Row>
+            <Row type="flex" justify="center">
+              <Col xs={24} lg={24}>
+                <div className="grey-background">
+                  <Title level={2}>Precios de lonja</Title>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+          <Col xs={2} lg={8}></Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default App;
