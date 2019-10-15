ec18523a51d8de5009938e91d24116bc3d203d54
rgarcia <rgarcia@transparentcdn.com>
2019-10-15 11:50:59 +0200

    - Improve App.css - Refactor code. - Improve and update style. - Responsive design. - Change some texts. - Create a images folder for backgrounds.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Typography, Row, Col } from 'antd';
-import background from './background.jpg'
+import background from './images/background.jpg'
 import './App.css';
 
 class App extends Component {
@@ -11,34 +11,40 @@ class App extends Component {
       <div className="App" style={{ backgroundImage: `url(${background})` }}>
         <Row>
           <Col span={24}>
-            <Title>LA LONJA ONLINE ESPAÑOLA</Title>
-            <Title level={3}>Compra y vende productos agrícolas sin intermediarios</Title>
+            <Title className="title">LA LONJA ONLINE ESPAÑOLA</Title>
+            <Title level={2}>Compra y vende productos agrícolas sin intermediarios</Title>
           </Col>
         </Row>
         <Row>
-          <Col xs={2} lg={8}></Col>
-          <Col xs={20} lg={8}>
+          <Col xs={2} md={4} lg={8}></Col>
+          <Col xs={20} md={16} lg={8}>
             <Row type="flex" justify="center" gutter={16} >
               <Col span={12}>
-                <div className="container-link blue-background">
-                  <Title level={2}>Publicar demanda</Title>
-                </div>
+                <a href="https://www.google.es/">
+                  <div className="container-link blue-background">
+                    <Title level={2}>Ver ofertas</Title>
+                  </div>
+                </a>
               </Col>
               <Col span={12}>
-                <div className="container-link green-background" >
-                  <Title level={2}>Ofrecer producto</Title>
-                </div>
+                <a href="https://www.google.es/">
+                  <div className="container-link green-background" >
+                    <Title level={2}>Ofrecer producto</Title>
+                  </div>
+                </a>
               </Col>
             </Row>
             <Row type="flex" justify="center">
               <Col xs={24} lg={24}>
-                <div className="grey-background">
-                  <Title level={2}>Precios de lonja</Title>
-                </div>
+                <a href="https://www.google.es/">
+                  <div className="container-link grey-background">
+                    <Title level={2}>Precios de lonja</Title>
+                  </div>
+                </a>
               </Col>
             </Row>
           </Col>
-          <Col xs={2} lg={8}></Col>
+          <Col xs={2} md={4} lg={8}></Col>
         </Row>
       </div>
     );
